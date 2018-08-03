56799b042af6180cd57762b108c9c4fb0d0dc8ca
Chaofan Ma <chaofanma@hotmail.com>
2018-08-03 14:28:56 +0800

    check

--- check_all/check_all.c
+++ check_all/check_all.c
@@ -4,12 +4,16 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 
+MODULE_LICENSE("Dual BSD/GPL");
+
 static struct nf_hook_ops nfho;
 
 unsigned int 
-hook_func(void *priv,
+hook_func(const struct nf_hook_ops *ops,
           struct sk_buff *skb,
-          const struct nf_hook_state *state
+          const struct net_device *in,
+	  const struct net_device *out,
+	  int (*okfn)(struct sk_buff *)
           ) {
     printk(KERN_ALERT "A packet is received!\n");
     return NF_ACCEPT;
@@ -18,18 +22,24 @@ hook_func(void *priv,
 static int __init
 init_check(void) {
     nfho.hook = hook_func;
-    nfho.hooknum = NF_INET_PRE_ROUTING;
     nfho.pf = PF_INET;
     nfho.priority = NF_IP_PRI_FIRST;
+// At the first time, the following statement is used:
+//  nfho.hooknum = NF_IP_PRE_ROUTING;
+// However, the compiler complains that NF_IP_PRE_ROUTING
+// is not declared. This is because NF_IP_PRE_ROUTING is a
+// user-space macro. When build a module in kernel, the 
+// macro NF_INET_PRE_ROUTING is used as follows.
+    nfho.hooknum = NF_INET_PRE_ROUTING;
 
-    nf_register_net_hook(NULL, &nfho);
+    nf_register_hook(&nfho);
 
     return 0;
 }
 
 static void __exit
 clean_check(void) {
-    nf_unregister_net_hook(NULL, &nfho);
+    nf_unregister_hook(&nfho);
 }
 
 module_init(init_check);
